3c1af2c79ca78536f83a14b574ce71fc29775379
louispawaon <lpawaon@gmail.com>
2024-06-25 22:50:30 +0800

    feat (client): pokemon and home pages

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import PokemonList from '../components/Pokemon/PokemonList';
+import MainComponent from './Pokemon';
+
+const Home = () => {
+  return (
+    <>
+      <PokemonList />
+      <MainComponent/>
+    </>
+  );
+};
+
+export default Home;
+
