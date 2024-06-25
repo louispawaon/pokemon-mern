ccfd433a6c38c1cfaa3de56d4e44cfed8c831100
louispawaon <lpawaon@gmail.com>
2024-06-26 07:41:15 +0800

    refactor(client-navbar): added further styling to navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import RegisterModal from '../Auth/RegisterModal';
 import LoginModal from '../Auth/LoginModal';
 import { Link } from 'react-router-dom';
+import { IoLogIn, IoLogOut } from "react-icons/io5";
+import { FaUserPlus } from "react-icons/fa6";
 
 type Props = {
   isLoggedIn: boolean;
@@ -14,37 +16,35 @@ const Navbar: React.FC<Props> = ({ isLoggedIn, onLogout }) => {
 
   return (
     <>
-      <header className="bg-gray-800 p-4 flex items-center justify-between">
-        
-          <div className="text-white text-2xl font-semibold">
-            <Link to='/'>
-              Pokedex
-            </Link>
-          </div>
-        
-        <div className="flex items-center space-x-4">
+      <header className="bg-gray-800 p-4 flex items-center justify-between md:flex-row flex-col">
+        <div className="text-yellow-400 stroke-blue-700 text-2xl font-semibold font-pokemon">
+          <Link to='/'>
+            Pokedex
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4 md:space-x-8">
           {!isLoggedIn && (
             <>
               <button
-                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-3 rounded flex items-center gap-2"
                 onClick={() => setShowLoginModal(true)}
               >
-                Login
+                <IoLogIn />Login
               </button>
               <button
-                className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+                className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded flex items-center gap-2 "
                 onClick={() => setShowRegisterModal(true)}
               >
-                Register
+               <FaUserPlus /> Register
               </button>
             </>
           )}
           {isLoggedIn && (
             <button
-              className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+              className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded flex items-center gap-2"
               onClick={onLogout}
             >
-              Logout
+              <IoLogOut /> Logout
             </button>
           )}
         </div>
