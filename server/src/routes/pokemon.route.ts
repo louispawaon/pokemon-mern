90e2b6dcefe828ded2accb364c3d77d39e809a2d
louispawaon <lpawaon@gmail.com>
2024-06-25 22:42:30 +0800

    refactor (server-pokemonroute): fixed delete route, added route for all types

--- server/src/routes/pokemon.route.ts
+++ server/src/routes/pokemon.route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllPokemon, getPokemonById, createPokemon, updatePokemon, deletePokemon } from '../controllers/pokemon.controller';
+import { getAllPokemon, getPokemonById, createPokemon, updatePokemon, deletePokemon, getAllTypes } from '../controllers/pokemon.controller';
 import authenticateJWT from '../middleware/auth.middleware';
 
 const pokeRouter = express.Router();
 
 pokeRouter.get('/', getAllPokemon);
+pokeRouter.get('/types', getAllTypes);
 pokeRouter.get('/:id', getPokemonById);
 pokeRouter.post('/', authenticateJWT, createPokemon);
 pokeRouter.put('/:id', authenticateJWT, updatePokemon);
-pokeRouter.put('/:id', authenticateJWT, deletePokemon);
+pokeRouter.delete('/:id', authenticateJWT, deletePokemon);
 
 export default pokeRouter;
