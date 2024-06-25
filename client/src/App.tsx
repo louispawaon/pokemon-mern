eb52248d982f3e9e65a59ac9d35bc4fb0b60da5c
louispawaon <lpawaon@gmail.com>
2024-06-25 22:52:25 +0800

    feat (client): main files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext';
+import Home from './pages/Home';
+import PokemonDetails from './components/Pokemon/PokemonDetails';
+import Navbar from './components/Navbar/Navbar';
+import MainComponent from './pages/Pokemon';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoggedIn, logout } = useAuth();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Router>
+        <Navbar isLoggedIn={isLoggedIn} onLogout={logout} />
+        <Routes>
+          <Route path='/' Component={Home}/>
+          <Route path='/try' Component={MainComponent}/>
+          <Route path='/pokemon/:id' Component={PokemonDetails}/>
+        </Routes>
+      </Router>
+
   )
 }
 
 export default App
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar/Navbar';
+// import PokemonList from './components/Pokemon/PokemonList';
+// import { AuthProvider, useAuth } from './contexts/AuthContext';
+// import PokemonDetails from './components/Pokemon/PokemonDetails';
+
+
+// const App = () => {
+//   const { isLoggedIn, logout } = useAuth();
+
+//   return (
+//     <Router>
+      
+//       <Routes>
+//         <Route path="/" element={<PokemonList />} />
+//         <Route path="/pokemon/:id" element={<PokemonDetails />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// const AppWithProvider = () => (
+//   <AuthProvider>
+//     <App />
+//   </AuthProvider>
+// );
+
+// export default AppWithProvider;
