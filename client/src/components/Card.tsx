b7645b47346cc8151bf0c483c17b155a0ec2aa36
louispawaon <lpawaon@gmail.com>
2024-06-26 07:41:00 +0800

    refactor(client-card): added styling to pokemon card

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -15,14 +15,16 @@ const PokemonCard: React.FC<PokemonCardProps> = ({
   artwork_url,
 }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <img className="w-full" src={artwork_url} alt={name} />
-      <div className="px-6 py-4">
-        <div className="flex justify-between items-center mb-2">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:shadow-2xl transition duration-300 ease-in-out">
+      <img
+        className="w-full object-cover"
+        src={artwork_url}
+        alt={name}
+      />
+      <div className="px-6 py-4 h-40">
+        <div className="mb-2">
           <div className="font-bold text-2xl text-gray-800 uppercase">{name}</div>
-          <div className="bg-gray-200 text-gray-700 font-semibold py-1 px-2 rounded-full">
-            EXP {base_exp}
-          </div>
+          <div className="text-gray-700 font-semibold">{`EXP ${base_exp}`}</div>
         </div>
         <div className="flex flex-wrap mb-2">
           {types.map((type) => (
@@ -34,7 +36,6 @@ const PokemonCard: React.FC<PokemonCardProps> = ({
             </span>
           ))}
         </div>
-        
       </div>
     </div>
   );
