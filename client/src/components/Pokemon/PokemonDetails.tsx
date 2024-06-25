dc7a47aa9db5a69377e08529a82f2e440951f34a
louispawaon <lpawaon@gmail.com>
2024-06-26 07:39:27 +0800

    refactor(client): changed pokemon list and details - added add pokemon to pokemon list - pokemon details with a better look

--- client/src/components/Pokemon/PokemonDetails.tsx
+++ client/src/components/Pokemon/PokemonDetails.tsx
@@ -10,7 +10,7 @@ import { PokemonType } from '../../utils/typePokemon';
 
 const PokemonDetails = () => {
   const { id } = useParams<{ id: string }>();
-  const {isLoggedIn, token} = useAuth();
+  const { isLoggedIn, token } = useAuth();
   const [pokemon, setPokemon] = useState<PokemonType | null>(null);
   const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const navigate = useNavigate();
@@ -24,17 +24,13 @@ const PokemonDetails = () => {
     }
   };
 
-
   useEffect(() => {
     fetchPokemon();
   }, [id]);
-  
-  console.log(token)
 
   const handleUpdate = () => {
-    navigate(`/pokemon/${id}`)
+    navigate(`/pokemon/${id}`);
     fetchPokemon();
-    console.log("yey!")
   };
 
   const handleDelete = async () => {
@@ -44,7 +40,7 @@ const PokemonDetails = () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      navigate('/'); 
+      navigate('/');
     } catch (error) {
       console.error('Failed to delete Pokémon:', error);
     }
@@ -53,71 +49,73 @@ const PokemonDetails = () => {
   if (!pokemon) return <div>Loading...</div>;
 
   return (
-    <div className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-lg">
-    <div className="flex flex-col md:flex-row items-center">
-      <img
-        className="w-[50%] object-cover mb-4 md:mb-0 md:mr-6"
-        src={pokemon.artwork_url}
-        alt={pokemon.name}
-      />
-      <div className="flex-1">
-        <div className="flex justify-between items-center mb-2">
-          <h1 className="text-3xl font-bold text-gray-800 uppercase">{pokemon.name}</h1>
-          <div className="bg-gray-200 text-gray-700 font-semibold py-1 px-2 rounded-full">
-            EXP {pokemon.exp}
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-lg">
+        <div className="flex flex-col md:flex-row items-center">
+          <img
+            className="w-full md:w-[50%] object-cover mb-4 md:mb-0 md:mr-6"
+            src={pokemon.artwork_url}
+            alt={pokemon.name}
+          />
+          <div className="flex-1">
+            <div className="flex justify-between items-center mb-2">
+              <h1 className="text-3xl font-bold text-gray-800 uppercase">{pokemon.name}</h1>
+              <div className="bg-gray-200 text-gray-700 font-semibold py-1 px-2 rounded-full">
+                EXP {pokemon.exp}
+              </div>
+            </div>
+            <p className="text-gray-700 text-base mb-2">
+              <strong>Types:</strong>{' '}
+              {pokemon.types.map((type: any) => (
+                <span
+                  key={type}
+                  className={`mr-2 px-2 py-1 rounded-sm ${typeColors[type.name] || 'bg-gray-400 text-white'}`}
+                >
+                  {type.name.charAt(0).toUpperCase() + type.name.slice(1)}
+                </span>
+              ))}
+            </p>
+            <p className="text-gray-700 text-base mb-2">
+              <strong>Abilities:</strong>{' '}
+              {pokemon.abilities.map((ability: string) => (
+                <span key={ability} className="mr-2 capitalize">
+                  {ability.charAt(0).toUpperCase() + ability.slice(1)}
+                </span>
+              ))}
+            </p>
+            <p className="text-gray-700 text-base mb-2">
+              <strong>Height:</strong> {pokemon.height}
+            </p>
+            <p className="text-gray-700 text-base mb-2">
+              <strong>Weight:</strong> {pokemon.weight}
+            </p>
+            {isLoggedIn && (
+              <div className="flex space-x-2 mt-2">
+                <button
+                  onClick={() => setIsUpdateModalOpen(true)}
+                  className="bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-4 rounded"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="bg-red-500 hover:bg-red-600 text-white py-1 px-4 rounded"
+                >
+                  Delete
+                </button>
+              </div>
+            )}
           </div>
         </div>
-        <p className="text-gray-700 text-base mb-2">
-        <strong>Types:</strong>{' '}
-            {pokemon.types.map((type: any) => (
-              <span
-                key={type}
-                className={`mr-2 px-2 py-1 rounded-sm ${typeColors[type.name] || 'bg-gray-400 text-white'}`}
-              >
-                {type.name.charAt(0).toUpperCase() + type.name.slice(1)}
-              </span>
-            ))}
-        </p>
-        <p className="text-gray-700 text-base mb-2">
-          <strong>Abilities:</strong>{' '}
-          {pokemon.abilities.map((ability: string) => (
-            <span key={ability} className="mr-2 capitalize">
-              {ability.charAt(0).toUpperCase() + ability.slice(1)}
-            </span>
-          ))}
-        </p>
-        <p className="text-gray-700 text-base mb-2">
-          <strong>Height:</strong> {pokemon.height}
-        </p>
-        <p className="text-gray-700 text-base mb-2">
-          <strong>Weight:</strong> {pokemon.weight}
-        </p>
       </div>
-      {isLoggedIn && (
-          <div className="flex space-x-2 mt-4">
-            <button
-              onClick={() => setIsUpdateModalOpen(true)}
-              className="bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-4 rounded"
-            >
-              Update
-            </button>
-            <button
-              onClick={handleDelete}
-              className="bg-red-500 hover:bg-red-600 text-white py-1 px-4 rounded"
-            >
-              Delete
-            </button>
-          </div>
-        )}
-    </div>
-    <Modal isOpen={isUpdateModalOpen} onClose={() => setIsUpdateModalOpen(false)}> {/* Use Modal component */}
+      <Modal isOpen={isUpdateModalOpen} onClose={() => setIsUpdateModalOpen(false)}>
         <UpdatePokemonForm
           pokemonId={id!}
           onClose={() => setIsUpdateModalOpen(false)}
           onSave={handleUpdate}
         />
-    </Modal>
-  </div>
+      </Modal>
+    </div>
   );
 };
 
--- client/src/components/Pokemon/PokemonList.tsx
+++ client/src/components/Pokemon/PokemonList.tsx
@@ -3,9 +3,16 @@ import { useEffect, useState } from 'react';
 import API from '../../services/api';
 import { Link } from 'react-router-dom';
 import PokemonCard from '../Card';
+import { useNavigate } from 'react-router-dom';
+import Modal from '../Form/Modal';
+import CreatePokemonForm from '../Form/CreatePokemon';
+import { useAuth } from '../../contexts/AuthContext';
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+  const {isLoggedIn} = useAuth();
 
   const fetchPokemons = async () => {
     try {
@@ -20,11 +27,34 @@ const PokemonList = () => {
     fetchPokemons();
   }, []);
 
-  fetchPokemons()
+  const handleSavePokemon = () => {
+    fetchPokemons();
+    setIsModalOpen(false);
+    navigate('/');
+  };
+
+  if (!pokemons) return <div>Loading...</div>;
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center capitalize">
+      <div className="flex justify-end mb-4">
+        {isLoggedIn ? (
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <i className="fas fa-plus mr-2" /> Create Pokémon
+          </button>
+        ) : (
+          <p className="text-gray-500 text-sm">
+            Please log in to create a Pokémon.
+          </p>
+        )}
+      </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <CreatePokemonForm onClose={() => setIsModalOpen(false)} onSave={handleSavePokemon} />
+      </Modal>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-items-center items-center">
         {pokemons.map((pokemon: any) => (
           <Link to={`/pokemon/${pokemon._id}`} key={pokemon._id}>
             <PokemonCard
