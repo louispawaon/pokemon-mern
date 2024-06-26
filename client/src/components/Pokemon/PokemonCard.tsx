def5667e6038316c2698f56adbaac9c8e3cc0f22
louispawaon <lpawaon@gmail.com>
2024-06-26 12:20:36 +0800

    refactor(client): rename Card to PokemonCard and placing it to Pokemon folder

--- client/src/components/Pokemon/PokemonCard.tsx
+++ client/src/components/Pokemon/PokemonCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { typeColors } from '../utils/typeColors';
+import { typeColors } from '../../utils/typeColors';
 
 interface PokemonCardProps {
   name: string;
@@ -15,22 +15,22 @@ const PokemonCard: React.FC<PokemonCardProps> = ({
   artwork_url,
 }) => {
   return (
-    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white hover:shadow-2xl transition duration-300 ease-in-out">
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-pokemonBlue-700 hover:shadow-2xl transition duration-300 ease-in-out text-white">
       <img
-        className="w-full object-cover border-4 border-yellow-400 rounded-t-lg"
+        className="w-full object-cover border-8 border-pokemonGold-500 bg-pokemonYellow-400 rounded-t-lg"
         src={artwork_url}
         alt={name}
       />
       <div className="px-6 py-4 h-40">
         <div className="mb-2">
-          <div className="font-bold text-2xl text-gray-800 capitalize font-gillSans">{name}</div>
-          <div className="text-gray-700 font-semibold">{`EXP ${base_exp}`}</div>
+          <div className="font-bold text-3xl capitalize font-gillSans">{name}</div>
+          <div className="text-pokemonGold-600 font-semibold text-xl">{`EXP ${base_exp}`}</div>
         </div>
         <div className="flex flex-wrap mb-2">
           {types.map((type) => (
             <span
               key={type}
-              className={`py-1 px-2 mr-4 mb-2 rounded-sm text-sm font-semibold capitalize ${typeColors[type] || 'bg-gray-300 text-black'}`}
+              className={`py-1 px-2 mr-4 mb-2 rounded-sm text-base font-semibold capitalize ${typeColors[type] || 'bg-gray-300 text-black'}`}
             >
               {type}
             </span>
